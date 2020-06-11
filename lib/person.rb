7ec53e87cdd22c1d7e9cb5d6cb27f0492f40202a
Jeff Bai <j.bai@wustl.edu>
2020-06-11 07:29:08 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,5 @@
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
-  
+
+  def initialize(name:nil, birthday:nil, hair_color:nil, height:nil, weight:nil, handed:nil, complexion:nil, t_shirt_size:nil, wrist_size:nil, glove_size:nil, pant_length:nil, pant_width:nil)
 end
