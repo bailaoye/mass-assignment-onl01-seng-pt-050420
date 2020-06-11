f20f88c1e1715cfe4a2bb73e100f718ee4c9b922
Jeff Bai <j.bai@wustl.edu>
2020-06-11 07:32:34 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,23 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(name:nil, birthday:nil, hair_color:nil, height:nil, weight:nil, handed:nil, complexion:nil, t_shirt_size:nil, wrist_size:nil, glove_size:nil)
-    attributes.each {|key, val| self.send("#{key}=", value)}
+    @name = name
+    @birthday = birthday
+    @hair_color = hair_color
+    @height = height
+    @weight = weight
+    @handed = handed
+    @complexion = complexion
+    @t_shirt_size = t_shirt_size
+    @wrist_size = wrist_size
+    @glove_size = glove_size
+    @pant_length = pant_length
+    @pant_width = pant_width
+  end
+
+  def add_attributes(pant_length:nil, pant_width:nil)
+    @pant_length = pant_length
+    @pant_width = pant_width
   end
 
 end
